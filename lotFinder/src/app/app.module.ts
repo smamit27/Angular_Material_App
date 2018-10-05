f387cff552332919f3b1aacfc4dcbce4671bb255
Amit Singh <amit.singh1@zensar.com>
2018-10-05 15:14:57 +0530

    checkin All

--- lotFinder/src/app/app.module.ts
+++ lotFinder/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {MaterialModule} from './material/material.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
+import {EmployeeService} from './shared/employee.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    EmployeeService
   ],
   imports: [
     BrowserModule,
